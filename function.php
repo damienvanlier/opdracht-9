2b3340c49a8ade2838310f0a1b601d2b22d25088
damienvanlier <9014287@student.zadkine.nl>
2023-02-20 13:21:15 +0100

    php9

--- function.php
+++ function.php
@@ -1,18 +1,53 @@
 <?php
+// Functie: Algemene functies voor 'main_bieren.php'.
+// Function 'ConnectDb' connect de database.
+function ConnectDb(){
+    try{
+        $conn = new PDO("mysql:host=localhost;dbname=bieren", "root", "");
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $query = $conn->prepare("SELECT * FROM bier");
+        $query->execute();
 
-function OpenDb(){
-    $db = new PDO('mysql:host=localhost;dbname=bieren', 'root', '');
- }   
+        global $result;
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
 
+        echo "Connected!";
+    } 
+    
+    catch(PDOException $e){
+        echo "Connection failed: " . $e->getMessage();
+    }
 
-function OvzBieren(){
-   $conn = openDb();
+    echo "<br>";
+    return $conn;
 }
 
+// Function 'OvzBieren' print het overzicht van de bieren.
+function OvzBieren(){
+    echo "Overzicht van alle bieren: ";
+    echo "<br>";
+    echo "<br>";
 
-
-function connectDb(){
-    echo "Function DoeIets<br>";
+    echo"<table>";
+    echo "<tr>";
+        echo "<td>" . "Bier Code" . "</td>";
+        echo "<td>" . "Bier Naam" . "</td>";
+        echo "<td>" . "Soort" . "</td>";
+        echo "<td>" . "Stijl" . "</td>";
+        echo "<td>" . "Alcohol %" . "</td>";
+    echo "</tr>";
+    global $result;
+    foreach($result as $data) {
+        echo "<tr>";
+            echo "<td>" . $data["biercode"] . "</td>";
+            echo "<td>" . $data["naam"] . "</td>";
+            echo "<td>" . $data["soort"] . "</td>";
+            echo "<td>" . $data["stijl"] . "</td>";
+            echo "<td>" . $data["alcohol"] . "</td>";
+        echo "</tr>";
+    }
+    echo"<table>";
+    echo "<br>";
 }
 
 // $servername = "localhost";
